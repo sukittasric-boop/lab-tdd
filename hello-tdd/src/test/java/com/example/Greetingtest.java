0662246d50951640236027c304007145669be164
sukittasric-boop <sukitta.sric@bumail.net>
2025-11-02 22:57:42 +0700

    Add files via upload

---
+++ hello-tdd/src/test/java/com/example/Greetingtest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetingTest {
+    @Test
+    void testHelloBasic() {
+        Greeting greeting = new Greeting();
+        String result = greeting.sayHello("World");
+        assertEquals("Hello, World", result);
+    }
+    @Test
+    void testHelloNull() {
+        Greeting greeting = new Greeting();
+        String result = greeting.sayHello(null);
+        assertEquals("Hello, Friend", result);
+    }
+
+}
